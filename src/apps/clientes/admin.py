eaafa60e30ab3391b14c33d056d31903d8bd880c
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 16:50:16 -0300

    Entidades no CMS modificadas

--- src/apps/clientes/admin.py
+++ src/apps/clientes/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Client, Product
+from .models import Product, Client
 
-admin.site.register(Client)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    list_display_links = ['title']
+    list_editable = ['price']
+    list_filter = ['brand']
+    search_fields = ['title']
+    save_on_top = True
+
+    class Meta:
+        model = Product
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+    list_display_links = ['name']
+    list_filter = ['name']
+    search_fields = ['email']
+    save_on_top = True
+
+    class Meta:
+        model = Client
