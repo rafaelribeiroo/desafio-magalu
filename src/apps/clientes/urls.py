d654e90d25ed9ce186f9c9da36f3b20329dda05a
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 19:29:17 -0300

    Comentando código

--- src/apps/clientes/urls.py
+++ src/apps/clientes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import CategoryViewSet
 
+# O trailing_slash serve para ignorarmos a barra final ao testar no Postman,
+# entre outros
 router = routers.DefaultRouter(trailing_slash=False)
+# Aqui vai a rota
 router.register('clients', CategoryViewSet)
 
 '''
