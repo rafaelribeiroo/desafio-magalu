ffcd8ab85e71d3b370ad6579e95acb666601c934
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 13:06:04 -0300

    Iniciando com restframework

--- src/urls.py
+++ src/urls.py
@@ -22,5 +22,6 @@ from src.apps.clientes import urls as clientes_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('clientes/', include((clientes_urls), namespace='core'))
 ]
