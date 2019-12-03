6ab4d9f75c059d019fb19ef8863037b496699488
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 14:45:03 -0300

    Comentado a funcionalidade de autenticação da API por ora

--- src/urls.py
+++ src/urls.py
@@ -22,6 +22,6 @@ from src.apps.clientes import urls as clientes_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
     path('api/', include((clientes_urls), namespace='core'))
 ]
