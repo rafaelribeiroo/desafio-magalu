f9457dc5db01a31ca8d9eb68737abe05070d22e6
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-02 22:35:48 -0300

    Mudança de banco de dados padrão, de SQLite3 para Postgres11

--- src/settings.py
+++ src/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': decouple('NAME_DB'),
+        'USER': decouple('USER_DB'),
+        'PASSWORD': decouple('PASSWD_DB'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
