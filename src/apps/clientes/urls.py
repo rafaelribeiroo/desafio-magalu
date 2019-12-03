348ff53a14806a6eeee9e42c4e64e22bf27b9930
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-02 23:09:44 -0300

    Múltiplas URLs para facilitar a visualização

---
+++ src/apps/clientes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import (
+#     view
+# )
+
+app_name = 'clientes'
+urlpatterns = [
+    # path('', <view>, name='<view_namespace>'),
+]
--- src/urls.py
+++ src/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# URLs externas
+from src.apps.clientes import urls as clientes_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clientes/', include((clientes_urls), namespace='core'))
 ]
