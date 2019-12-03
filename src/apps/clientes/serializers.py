6cd2d7c340c1c655606f16ed1cc9c7c22aac8038
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 13:45:53 -0300

    Criação do arquivo serializers e explicação rápida do conceito técnico

---
+++ src/apps/clientes/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+'''
+O que é a serialização? Serializar uma informação seria...
+Simplificando ao máximo, seria "converter" informações e modelos em JSON.
+'''
