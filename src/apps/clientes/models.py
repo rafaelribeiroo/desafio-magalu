abf027f1d4ea51c63a5578547716665e8b3ef0b8
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-02 23:50:18 -0300

    Entidade Product criada, dentro das normas PEP-8

--- src/apps/clientes/models.py
+++ src/apps/clientes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    # Por padrão, o django já cria uma coluna de id
+    title = models.CharField(
+        'Título',
+        max_length=50,
+        unique=True,
+        # A nível DB
+        null=False,
+        # ... template
+        blank=True
+    )
+    price = models.DecimalField(
+        'Preço',
+        max_digits=6,
+        decimal_places=2,
+        null=False
+    )
+    image = models.URLField('URL da imagem', max_length=200, null=False)
+    brand = models.CharField('Marca', max_length=50, null=False)
+    reviewScore = models.DecimalField(
+        'Pontuação do produto',
+        max_digits=3,
+        decimal_places=1
+    )
