eaaa97fc739c7d9275c3ce7204777d0ef9e8209c
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 23:10:16 -0300

    Filtrando o que será enviado à API

--- src/apps/clientes/views.py
+++ src/apps/clientes/views.py
@@ -1,11 +1,16 @@
-from .serializers import ClientSerializer
+from .serializers import ClientSerializer, ProductSerializer
 from rest_framework import viewsets
-from .models import Client
+from .models import Client, Product
 
 # Ao disponibilizar um recurso, a classe é mais apropriada que várias funções,
 # pois ficam agrupadas todas as operações que oferecemos.
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class ClientViewSet(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
