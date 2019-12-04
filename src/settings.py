4fa35b259e43b0aaf679a3ee1a6d38f66040afb3
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-04 01:15:26 -0300

    Paginação iniciada

--- src/settings.py
+++ src/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CO DE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
