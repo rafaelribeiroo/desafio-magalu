a812b279582f98f7373a50e6136ef558f449e3fd
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 23:10:35 -0300

    Rotas sincronizadas

--- src/apps/clientes/urls.py
+++ src/apps/clientes/urls.py
@@ -1,12 +1,13 @@
+from .views import ClientViewSet, ProductViewSet
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoryViewSet
 
 # O trailing_slash serve para ignorarmos a barra final ao testar no Postman,
 # entre outros
 router = routers.DefaultRouter(trailing_slash=False)
 # Aqui vai a rota
-router.register('clients', CategoryViewSet)
+router.register('clients', ClientViewSet)
+router.register('product', ProductViewSet)
 
 '''
 Qual a diferença entre put e patch?
