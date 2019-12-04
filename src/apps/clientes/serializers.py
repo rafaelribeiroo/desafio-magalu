954923ab26461231a95b8f9cbfa3e097435525b1
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-03 23:09:41 -0300

    Relação muitos-para-muitos no serializers

--- src/apps/clientes/serializers.py
+++ src/apps/clientes/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Client
+from .models import Client, Product
 
 '''
 O que é a serialização? Serializar uma informação seria...
@@ -7,8 +7,27 @@ Simplificando ao máximo, seria "converter" informações e modelos em JSON.
 '''
 
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        # fields = '__all__'
+        fields = ('title', 'price', 'image', 'brand', 'reviewScore')
+
+
 class ClientSerializer(serializers.ModelSerializer):
+    # favorite = ProductSerializer(read_only=True, many=True)
+    '''favorite_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        write_only=True,
+        source='favorite',
+        many=True
+    )'''
+    favorite = serializers.PrimaryKeyRelatedField(
+        queryset=Client.objects.all(),
+        many=True
+    )
+
     class Meta:
         model = Client
-        # Diretiva que reune todas as colunas nesse caso
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('id', 'name', 'email', 'favorite')  # favorite_id
