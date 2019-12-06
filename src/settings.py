07b82010b82e84c9de85d8bb6fa13da280294941
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-12-06 15:11:32 -0300

    Aplicado makemigrations e migrate]

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = decouple('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = decouple('DEBUG', default=True, cast=bool)
+DEBUG = decouple('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['maga-api.herokuapp.com']
 
